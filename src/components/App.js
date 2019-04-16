fdd584043b8b2050e91e9459c90d06f087bda77b
minye.wu <minye.wu.cs@gmail.com>
2019-04-16 00:31:41 -0500

    imporve format and add background images

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,22 @@
 import React, { Component } from "react";
 import "../css/App.css";
 
-import AddPlayer from './AddPlayer';
-import SearchPlayer from './SearchPlayer';
-import ListPlayer from './ListPlayer';
+import AddPlayer from "./AddPlayer";
+import SearchPlayer from "./SearchPlayer";
+import ListPlayer from "./ListPlayer";
 
-import {without} from 'lodash';
-import {findIndex} from 'lodash';
+import { without } from "lodash";
+import { findIndex } from "lodash";
 
 class App extends Component {
-
   constructor() {
     super();
     this.state = {
       myRoster: [],
-      orderBy: 'playerName',
-      orderDirection: 'ascending',
-      queryText: '',
-      formDisplay: true,
+      orderBy: "playerName",
+      orderDirection: "ascending",
+      queryText: "",
+      formDisplay: false,
       lastIndex: 0
     };
     this.deletePlayer = this.deletePlayer.bind(this);
@@ -64,7 +63,7 @@ class App extends Component {
 
   toggleForm() {
     this.setState({
-      formDisplay: !this.state.formDisplay   
+      formDisplay: !this.state.formDisplay
     });
   }
 
@@ -78,35 +77,49 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch('./data.json')
+    fetch("./data.json")
       .then(response => response.json())
       .then(result => {
         const player = result.map(item => {
           item.playerID = this.state.lastIndex;
-          this.setState({ lastIndex : this.state.lastIndex + 1});
+          this.setState({ lastIndex: this.state.lastIndex + 1 });
           return item;
         });
         this.setState({
           myRoster: player
         });
-      })
+      });
   }
 
   render() {
-
     let tempOrder;
     let filteredRoster = this.state.myRoster;
-    this.state.orderDirection === 'ascending' ? tempOrder = 1 : tempOrder = -1;
-    filteredRoster = filteredRoster.sort((a,b) => {
-      return a[this.state.orderBy].toLowerCase() < b[this.state.orderBy].toLowerCase() ? tempOrder * -1 : tempOrder * 1;
-    }).filter(item => {
-      return (
-        item['playerName'].toLowerCase().includes(this.state.queryText.toLowerCase()) ||
-        item['birthday'].toLowerCase().includes(this.state.queryText.toLowerCase()) ||
-        item['agent'].toLowerCase().includes(this.state.queryText.toLowerCase()) ||
-        item['scoutingReport'].toLowerCase().includes(this.state.queryText.toLowerCase())
-      )
-    });
+    this.state.orderDirection === "ascending"
+      ? (tempOrder = 1)
+      : (tempOrder = -1);
+    filteredRoster = filteredRoster
+      .sort((a, b) => {
+        return a[this.state.orderBy].toLowerCase() <
+          b[this.state.orderBy].toLowerCase()
+          ? tempOrder * -1
+          : tempOrder * 1;
+      })
+      .filter(item => {
+        return (
+          item["playerName"]
+            .toLowerCase()
+            .includes(this.state.queryText.toLowerCase()) ||
+          item["birthday"]
+            .toLowerCase()
+            .includes(this.state.queryText.toLowerCase()) ||
+          item["agent"]
+            .toLowerCase()
+            .includes(this.state.queryText.toLowerCase()) ||
+          item["scoutingReport"]
+            .toLowerCase()
+            .includes(this.state.queryText.toLowerCase())
+        );
+      });
 
     return (
       <main className="page bg-white" id="petratings">
@@ -114,22 +127,26 @@ class App extends Component {
           <div className="row">
             <div className="col-md-12 bg-white">
               <div className="container">
-                <AddPlayer 
-                  formDisplay = {this.state.formDisplay}
-                  toggleForm = {this.toggleForm}
-                  addPlayer = {this.addPlayer}
-                />
-                <SearchPlayer 
-                  orderBy = {this.state.orderBy}
-                  orderDirection = {this.state.orderDirection}
-                  selectOrder = {this.selectOrder}
-                  searchPlayer = {this.searchPlayer}
-                />
-                <ListPlayer 
-                  roster = {filteredRoster}
-                  deletePlayer = {this.deletePlayer} 
-                  modify = {this.modify}
-                />
+                <div className="App-component">
+                  <div className="App-component2">
+                    <AddPlayer
+                      formDisplay={this.state.formDisplay}
+                      toggleForm={this.toggleForm}
+                      addPlayer={this.addPlayer}
+                    />
+                    <SearchPlayer
+                      orderBy={this.state.orderBy}
+                      orderDirection={this.state.orderDirection}
+                      selectOrder={this.selectOrder}
+                      searchPlayer={this.searchPlayer}
+                    />
+                    <ListPlayer
+                      roster={filteredRoster}
+                      deletePlayer={this.deletePlayer}
+                      modify={this.modify}
+                    />
+                  </div>
+                </div>
               </div>
             </div>
           </div>
